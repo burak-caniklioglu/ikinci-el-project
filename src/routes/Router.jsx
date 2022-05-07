337b74ac334e50d8bdfecf06513f376ddb6358dc
Burak Caniklioğlu <burakcaniklioglu@gmail.com>
2022-05-08 02:22:20 +0300

    Added lazy and suspense

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Account from '../pages/Account';
-import AddProduct from '../pages/Add-Product';
-import Login from '../pages/LoginRegister';
-import Main from '../pages/Main';
-import NotFound from '../pages/Not-Found';
-import ProductDetail from '../pages/Product-Detail';
+import Loading from '../components/Loading';
 import PrivateRouter from './PrivateRouter';
+
+const Main = lazy(() => import('../pages/Main'));
+const Login = lazy(() => import('../pages/LoginRegister'));
+const Account = lazy(() => import('../pages/Account'));
+const ProductDetail = lazy(() => import('../pages/Product-Detail'));
+const AddProduct = lazy(() => import('../pages/Add-Product'));
+const NotFound = lazy(() => import('../pages/Not-Found'));
 
 function Router() {
   return (
 
-    <Routes>
-      <Route path="/" element={<Main />} exact />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Main />} exact />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
 
-      <Route element={<PrivateRouter />}>
-        <Route path="/myaccount" element={<Account />} />
-        <Route path="/productdetail/:id" element={<ProductDetail />} />
-        <Route path="/addproduct" element={<AddProduct />} />
-      </Route>
+        <Route element={<PrivateRouter />}>
+          <Route path="/myaccount" element={<Account />} />
+          <Route path="/productdetail/:id" element={<ProductDetail />} />
+          <Route path="/addproduct" element={<AddProduct />} />
+        </Route>
 
-    </Routes>
+      </Routes>
+    </Suspense>
 
   );
 }
