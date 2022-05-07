31518525746388d9b90e0fc553bace3817754486
Burak Caniklioğlu <burakcaniklioglu@gmail.com>
2022-05-08 01:51:56 +0300

    StrictMode removed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-filename-extension */
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.scss';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,11 +7,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
   document.getElementById('root'),
 );
 
