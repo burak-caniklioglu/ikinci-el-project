adc919aea0cb539d165e187f0bf2fb87a508523d
Burak Caniklioğlu <burakcaniklioglu@gmail.com>
2022-05-08 11:19:56 +0300

    Not found page is ready

--- src/pages/Not-Found/index.jsx
+++ src/pages/Not-Found/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
+import attention from '../../constants/icons/attention.png';
+import './notfound.scss';
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <Navbar />
+      <div className="error-container">
+        <div className="error-wrapper">
+          <div className="error-img">
+            <img src={attention} alt="errorImg" />
+          </div>
+          <div className="error-message">
+            <p>Oooops! </p>
+            Aradığınız sayfa bulunamadı.
+            {' '}
+            <Link to="/">
+              <div className="error-link">Anasayfaya dönmek için tıklayın...</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
