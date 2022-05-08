008a6c7781b793becd14da0c3078fc98ca23718c
Burak Caniklioğlu <burakcaniklioglu@gmail.com>
2022-05-08 10:08:51 +0300

    robots.txt added

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: 'https://protein-patika-graduation-burak-caniklioglu.netlify.app/',
-  },
-  plugins: ['gatsby-plugin-robots-txt', 'gatsby-plugin-react-helmet'],
-};
